5639d668cf3dbc286f906168c5e7b36d12efa5de
nbsdeveloper-lewis <75124231+nbsdeveloper-lewis@users.noreply.github.com>
2023-04-22 09:26:11 +0200

    Added Logging

---
+++ Common.Libraries.Services.Logging.SeriLog/SeriLogger.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CommonLibraries.Application.Logging
+{
+    public static class SeriLogger
+    {
+        public static Action<HostBuilderContext, LoggerConfiguration> Configure =>
+           (context, configuration) =>
+           {
+               var elasticUri = context.Configuration.GetValue<string>("ElasticConfiguration:Uri");
+               var dir = Directory.GetCurrentDirectory();
+               configuration
+                    .Enrich.FromLogContext()
+                    .Enrich.WithMachineName()
+                    //.WriteTo.Debug()
+                    .WriteTo.File($"{dir}\\Logs\\log_{DateTime.Now.ToString("yyyy_mm_dd_HH_mm")}.txt", rollingInterval: RollingInterval.Day)
+                    .WriteTo.Console()
+                    .WriteTo.Elasticsearch(
+                        new ElasticsearchSinkOptions(new Uri(elasticUri))
+                        {
+                            IndexFormat = $"vasportalmicroserviceslogs-{context.HostingEnvironment.ApplicationName?.ToLower().Replace(".", "-")}-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}",
+                            AutoRegisterTemplate = true,
+                            //NumberOfShards = 2,
+                           // NumberOfReplicas = 1
+                        })
+                    .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+                    .Enrich.WithProperty("Application", context.HostingEnvironment.ApplicationName)
+                    .ReadFrom.Configuration(context.Configuration);
+           };
+    }
+}
